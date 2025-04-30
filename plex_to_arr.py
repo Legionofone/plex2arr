465eabf93875ffc66a7289bddbe5e3b6e73c33fb
Legionofone <richard@rhc2.com>
2025-04-30 14:47:28 -0500

    Update plex_to_arr.py

--- plex_to_arr.py
+++ plex_to_arr.py
@@ -50,7 +50,6 @@ def get_language(URL, API_KEY):
     response = requests.get(language_url)
     if response.status_code == 200:
         language_data = response.json()
-        print(language_data['uiLanguage'])
         return language_data['uiLanguage']
     else:
         print(f'Failed to language for {URL}. Status Code: {response.status_code}', flush=True )
